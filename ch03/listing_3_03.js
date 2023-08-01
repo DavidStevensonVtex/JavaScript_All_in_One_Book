cb81b4ea09b28b2f32da99b35d9972b7849fa7b9
David Stevenson <drstevenson1958@yahoo.com>
2023-08-01 05:27:05 -0400

    The Object Data Type

---
+++ ch03/listing_3_03.js
@@ -0,0 +1,10 @@
+const customer = {
+    name: 'Laura Wigfall',
+    address: '3427 Crummit Lane',
+    city: 'Providence',
+    state: 'RI',
+    zipcode: '02905',
+    customerId: 4,
+    isInLoyaltyClub: true,
+} ;
+console.log(customer);
