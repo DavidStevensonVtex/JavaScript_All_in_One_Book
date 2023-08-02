30879726fd8c60b294718282408f921edbc7fe88
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 05:10:48 -0400

    Chapter 4: Working with Operators and Expressions
    Strict vs Non-Strict Comparisons for Equality

---
+++ ch04/001_strict_vs_non-strict_comparisons.js
@@ -0,0 +1,20 @@
+if ( 0 == "0")
+{
+    console.log('0 is equal to "0" via ==');
+}
+else
+{
+    console.log('0 is not equal to "0" via ==');
+}
+
+if ( 0 === "0" )
+{
+    console.log('0 is equal to "0" via ===');
+}
+else
+{
+    console.log('0 is not equal to "0" via ===');
+}
+
+// 0 is equal to "0" via ==
+// 0 is not equal to "0" via ===
