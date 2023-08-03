e1fb297f50b00b4c95ae487d34876b58662c0960
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 08:56:47 -0400

    Spreading Arrays

---
+++ ch06/spreading_arrays.js
@@ -0,0 +1,7 @@
+const firstArray = [ 'a', 'b', 'c' ] ;
+const secondArray = [ ...firstArray, 'd' ] ;
+console.log("firstArray", firstArray);
+console.log("secondArray", secondArray) ;
+
+// firstArray [ 'a', 'b', 'c' ]
+// secondArray [ 'a', 'b', 'c', 'd' ]
