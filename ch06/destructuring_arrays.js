9fd9ca642deb637d99632dcb5e8d27cada96f209
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 08:55:15 -0400

    Destructuring Arrays

---
+++ ch06/destructuring_arrays.js
@@ -0,0 +1,8 @@
+const person = [ 'Russell C. Guy', '3447 Twin House Lane', 'Neosho', 'MO' ] ;
+let [personName, address, city, state ] = person ;
+console.log("personName", personName, "\naddress", address, "\ncity", city, "\nstate", state) ;
+
+// personName Russell C. Guy
+// address 3447 Twin House Lane
+// city Neosho
+// state MO
