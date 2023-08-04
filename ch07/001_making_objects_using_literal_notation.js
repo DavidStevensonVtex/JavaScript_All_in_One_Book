6e38d82ee26428459da4386ff44761d1f198d111
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 08:30:25 -0400

    Creating Objects: Making objects using literal notation

---
+++ ch07/001_making_objects_using_literal_notation.js
@@ -0,0 +1,8 @@
+// Using object literal notation to define an object.
+const person = { eyes: 2, feet: 2, eyeColor: 'brown' } ;
+
+// Initialize an object as an empty object and add properties.
+const person2 = {} ;
+person.hair = 'black' ;
+person.hands = 2 ;
+person.fullName = { firstName: 'Lamont', lastName: 'Rudnick' } ;
