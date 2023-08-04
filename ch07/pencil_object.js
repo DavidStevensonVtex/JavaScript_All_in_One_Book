1d7beaa2b3ff26301c3f59988110a8f669fadcab
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 08:27:06 -0400

    Use the Pencil object

--- ch07/pencil_object.js
+++ ch07/pencil_object.js
@@ -6,3 +6,10 @@ const pencil = {
     erase: function() { /* do erasing */ },
     sharpen: function() { /* do sharpening */ }
 }
+
+pencil.sharpen();
+
+pencil.length = "7 inches" ;
+pencil.sharpness = "sharp" ;
+
+const newPencil = { ...pencil } ;
