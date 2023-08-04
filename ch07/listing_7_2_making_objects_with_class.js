aeacc13556ae7465edf46952bd2ec3ce4169b1d9
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 08:41:17 -0400

    Listing 7-2. Making objects with class

---
+++ ch07/listing_7_2_making_objects_with_class.js
@@ -0,0 +1,12 @@
+class Pet {
+    constructor(name, type)
+    {
+        this.name = name ;
+        this.type = type ;
+    }
+}
+
+const ourDog = new Pet('Spencer', 'shichon' ) ;
+console.log(ourDog) ;
+
+// Pet { name: 'Spencer', type: 'shichon' }
