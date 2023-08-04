66b17746889bdd16b693cee0bdd8727e114c1216
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 08:25:26 -0400

    Chapter 7: Making an Using Objects

---
+++ ch07/pencil_object.js
@@ -0,0 +1,8 @@
+const pencil = {
+    length: "7.5 inches",
+    shape: "hexagonal",
+    daimeter: "1/4 inch",
+    write: function() { /* do writing */ },
+    erase: function() { /* do erasing */ },
+    sharpen: function() { /* do sharpening */ }
+}
