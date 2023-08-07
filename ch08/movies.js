be115393c7920c69d764b48029f737a78518bcaf
David Stevenson <drstevenson1958@yahoo.com>
2023-08-07 08:02:59 -0400

    Listing 8-8. A random movie picker app

---
+++ ch08/movies.js
@@ -0,0 +1,8 @@
+const movies = [
+    { title: 'Raiders of the Lost Ark' },
+    { title: 'Star Wars' },
+    { title: 'Back to the Future' },
+    { title: 'The Lord of the Rings: The Fellowship of the Ring' },
+    { title: 'Braveheart' },
+    { title: 'Cast Away' }
+]
