4ddf454a528ce749fc6cbd8d33e8111620146a3c
David Stevenson <drstevenson1958@yahoo.com>
2023-08-07 08:44:19 -0400

    Listing 8-13. A shopping cart

---
+++ ch08/listing_8_13_a_shopping_cart.js
@@ -0,0 +1,11 @@
+// Listing 8-13. A shopping cart
+
+const shoppingCart = {
+    itemsInCart: [ 'The Great Gatsby' ],
+    displayCart() {
+        const renderTarget = document.getElementById('cart') ;
+        const itemsInCart = this.itemsInCart.map(item => `<p>${item}{/p>`);
+        const checkoutButton = "<button id='checkout'>Check out</button>";
+        renderTarget.innerHTML = itemsInCart.join('') + checkoutButton;
+    }
+}
