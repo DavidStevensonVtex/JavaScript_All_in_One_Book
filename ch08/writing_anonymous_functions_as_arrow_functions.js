38f9c5f107b8e2cb04fdb537725c4c68b23ce030
David Stevenson <drstevenson1958@yahoo.com>
2023-08-07 07:44:11 -0400

    Simplifying arrow functions

--- ch08/writing_anonymous_functions_as_arrow_functions.js
+++ ch08/writing_anonymous_functions_as_arrow_functions.js
@@ -3,14 +3,15 @@ const pickAMovie1 = function(choices) {
     return myPick ;
 }
 
-const pickAMovie2 = (choices) => {
-    let myPick = choices[Math.floor(Math.random() * choices.length)];
-    return myPick ;
-}
+// If an arrow function takes only one parameter, you can eliminate the parentheses around the argument.
+// If an arrow function contains only a return statement, you can eliminate the curly braces.
+
+const pickAMovie2 = choices => 
+    choices[Math.floor(Math.random() * choices.length)];
 
 choices = [ 'one', 'two', 'three', 'four', 'five' ] ;
 console.log("pickAMovie1: ", pickAMovie1(choices)) ;
 console.log("pickAMovie2: ", pickAMovie2(choices)) ;
 
 // pickAMovie1:  two
-// pickAMovie2:  five
+// pickAMovie2:  three
