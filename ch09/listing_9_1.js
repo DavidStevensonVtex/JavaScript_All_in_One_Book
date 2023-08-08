0d4ac940adde31cad35ba57a6b4b4d1bb0528d57
David Stevenson <drstevenson1958@yahoo.com>
2023-08-08 09:35:18 -0400

    Listing 9-1. Using this in a class constructor

---
+++ ch09/listing_9_1.js
@@ -0,0 +1,8 @@
+// Listing 9-1. Using this in a class constructor
+
+class Beverage {
+    constructor ( size, temperature ) {
+        this.size = size ;
+        this.temperature = temperature ;
+    }
+}
