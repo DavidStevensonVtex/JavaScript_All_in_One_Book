43aad0f5bceb3df4260b6c43024a2dba1a58b2b3
David Stevenson <drstevenson1958@yahoo.com>
2023-08-12 12:32:13 -0400

    Listing 1-1. A Static Navigation Bar Mockup

---
+++ book_3_ch_01/NavBar.jsx
@@ -0,0 +1,16 @@
+// Listing 1-1. A Static Navigation Bar Mockup
+
+function TopNavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>All</li>
+                <li>News</li>
+                <li>Video</li>
+                <li>Images</li>
+                <li>Books</li>
+                <li>More</li>
+             </ul>
+        </nav>
+    )
+}
