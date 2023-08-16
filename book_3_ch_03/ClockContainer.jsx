f4a90c74f13e95a4ea5a8f829512a556348e965b
David Stevenson <drstevenson1958@yahoo.com>
2023-08-16 09:26:17 -0400

    Listing 3-16. A Function Component with a Timer

---
+++ book_3_ch_03/ClockContainer.jsx
@@ -0,0 +1,36 @@
+// Listing 3-16. A Function Component with a Timer
+
+import { useState, useEffect } from 'react' ;
+
+function ClockContainer(props) {
+    const [visible, setVisible] = useState(true);
+
+    return (
+        <div>
+            <button onClick={() => setVisible(!visible) }>Toggle Clock Visibility</button>
+            { visible && <Clock /> }
+        </div>
+    ) ;
+}
+
+function Clock(props) {
+    const [date, setDate] = useState(new Date()) ;
+    useEffect( () =>{
+        const timer = setInterval( () => getNewTime(), 1000);
+        return () => {
+            console.log('stopping the timer...');
+            clearInterval(timer) ;
+        };
+    }, []);
+
+    function getNewTime() {
+        this.setState( {date: new Date() });
+    }
+
+    return (
+        <h1>
+            The current time is {date.toLocaleTimeString()}
+        </h1>
+    );
+}
+
