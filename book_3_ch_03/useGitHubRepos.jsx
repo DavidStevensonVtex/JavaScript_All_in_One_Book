d4329f2438079765e618840dfd9a1de84fb7eceb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-16 10:09:16 -0400

    Listing 3-22: The useGitHubRepos Custom Hook.

---
+++ book_3_ch_03/useGitHubRepos.jsx
@@ -0,0 +1,28 @@
+// Listing 3-22: The useGitHubRepos Custom Hook.
+
+import { useState, useEffect } from 'react' ;
+
+export default function useGitHubRepos(username) {
+    const [repos, setRepos] = useState([]) ;
+    const [isLoading, setIsLoading] = useState('idle') ;
+
+    useEffect( () => {
+        const getRepos = async() => {
+            try 
+            {
+                setIsLoading(true) ;
+                let response = await fetch( `https://api.github.com/users/${username}repos`);
+                let data = await response.json() ;
+                setRepos(data);
+            }
+            catch ( error ) {
+                console.log(error);
+            }
+        } ;
+
+        getRepos() ;
+        setIsLoading(false);
+    }, [username] ) ;
+
+    return [repos, isLoading] ;
+}
